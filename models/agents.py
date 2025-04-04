97af998b24707278c264850c332a73d131bb8a46
Paraceratherium <adrian.kolodziejski@gmail.com>
2025-04-04 19:59:00 +0200

    start developing preDQNAgent

--- models/agents.py
+++ models/agents.py
@@ -1,18 +1,28 @@
 import torch
 import torch.nn as nn
+import random
+import numpy as np
 
 class preDQNAgent(nn.Module):
     '''
     A simple preDQN (i.e. does not use replay buffer) agent with a feedforward neural network architecture.
     '''
-    def __init__(self, epsilon):
+    def __init__(self, model, bootstrap, epsilon, gamma, optimizer):
         '''
         Args:
+            model (torch.nn.Module): The neural network model used by the agent.
+            bootstrap (str): either 'sarsa' or 'qlearning', indicating the type of bootstrapping used.
             epsilon (float): The exploration rate for the epsilon-greedy policy.
+            gamma (float): The discount factor for future rewards.
+            optimizer (torch.optim.Optimizer): The optimizer used for training the model.
         '''
         super().__init__(self)
         # Assign attributes
+        self.model = model
+        self.bootstrap = bootstrap
         self.epsilon = epsilon
+        self.gamma = gamma
+        self.optimizer = optimizer
         
     def select_greedy_action(self, state):
         '''
@@ -34,10 +44,11 @@ class preDQNAgent(nn.Module):
             state (torch.Tensor): The current state of the environment.
         '''
         pass
+        
 
     def train(self, num_epochs):
         '''
-        Implements training loop for the agent.
+        Implements training loop for the agent. During training exploratory actions are selected.
         
         Args:
             num_epochs (int): The number of training epochs.
