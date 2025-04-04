b243c8b5cb095b0db50037663a79afd79d8a8e4b
Paraceratherium <adrian.kolodziejski@gmail.com>
2025-04-04 17:29:58 +0200

    create model.py file

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,7 @@
 '''
 Contains all models used and tested in the project.
+
+agents.py contains the agent classes, which are responsible for selecting actions and learning from the environment.
+
+neural_nets.py contains the neural network architectures used by the agents.
 '''
