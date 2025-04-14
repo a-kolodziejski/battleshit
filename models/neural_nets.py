b18555e3c28573346d64ef13affaecfa48dbfc64
Paraceratherium <adrian.kolodziejski@gmail.com>
2025-04-14 09:44:34 +0200

    Implement DuellingFCN type of network

--- models/neural_nets.py
+++ models/neural_nets.py
@@ -1,6 +1,10 @@
 import torch
 import torch.nn as nn
 
+###################################################################################
+##                          SimpleFCN
+##################################################################################
+
 class SimpleFCN(torch.nn.Module):
     '''
     Implements fully connected neural network built from linear layers.
@@ -51,4 +55,65 @@ class SimpleFCN(torch.nn.Module):
         # Pass data through last linear layer
         outputs = self.output_activation(self.output_linear(inputs))
         # Return outputs
+        return outputs
+    
+###################################################################################
+##                          DuellingFCN
+##################################################################################
+
+class DuellingFCN(torch.nn.Module):
+    '''
+    Implements fully connected neural network suitable for Duelling DDQN algorithm.
+    '''
+    def __init__(self, input_dim, output_dim, hidden_dims,
+                 hidden_activation, output_activation):
+        '''
+        Args:
+            input_dim (int): dimensionality of input vector
+            output_dim (int): dimensionality of output vector (= number of actions)
+            hidden_dims (tuple of ints): dimensionality of hidden Linear layers
+            hidden_activation (torch.nn.functional): activation function for hidden layers
+            output_activation (torch.nn.functional): activation function for output layer
+        '''
+        super().__init__()
+        # Assign attributes and acivations
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.hidden_activation = hidden_activation
+        self.output_activation = output_activation
+        self.input_linear = torch.nn.Linear(in_features = input_dim, out_features = hidden_dims[0])
+        self.hidden_layers = torch.nn.ModuleList()
+        # Adding Linear layers to the list of hidden layers
+        for i in range(0, len(hidden_dims)-1):
+            self.hidden_layers.append(torch.nn.Linear(in_features = hidden_dims[i], out_features = hidden_dims[i+1]))
+        # Output for V function (output dim = 1)
+        self.output_v = torch.nn.Linear(in_features = hidden_dims[-1], out_features = 1)
+        # Output for advantage function
+        self.output_a = torch.nn.Linear(in_features = hidden_dims[-1], out_features = self.output_dim)
+    
+    def forward(self, inputs):
+        '''
+        Implements forward pass of data through the network
+
+        Args:
+            inputs (torch.tensor): input data of dimension (batch_size, input_dim)
+        '''
+        # Make sure inputs are of type torch.Tensor
+        if not isinstance(inputs, torch.Tensor):
+            inputs = torch.tensor(inputs, dtype = torch.float32)
+        # Make sure input is of the right dimension
+        if inputs.ndim == 1:
+            inputs = inputs.unsqueeze(0)
+        # Pass input through first linear layer
+        inputs = self.activation(self.input_linear(inputs))
+        # Pass input through list of hidden layers
+        for hidden_layer in self.hidden_layers:
+            inputs = self.activation(hidden_layer(inputs))
+        # Pass data through last linear layer - separately for V track and adbantage track
+        # For V
+        outputs_v = self.output_v(inputs)
+        # For A
+        outputs_a = self.output_a(inputs)
+        # Combining V and A function (Q = V + A - average(A))
+        outputs = outputs_v + outputs_a - torch.mean(outputs_a, dim = 1, keepdim = True)
         return outputs
